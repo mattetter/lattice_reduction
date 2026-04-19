73e7b5b9f66cf2d3c5739eda074dbb65a05492db
mattetter <mattetter1@gmail.com>
2026-04-19 14:46:25 -0600

    setting up header for matrix

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#ifndef MATRIX_H__
+#define MATRIX_H__
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+class Matrix {
+    private:
+        // a matrix is just a list of rows of doubles
+        vector<vector<double>> data;
+    public: 
+        // Constructor
+        Matrix(int rows, int cols);
+
+        // track number of rows/columns
+        int rows() const;
+        int cols() const;
+
+        // obtain dot product of two rows - need for LLL
+        double dot(int i, int j) const;
+
+
+        // swap row operation for LLL
+
+        // reduce row i by row j * scalar
+        void scale_row(int i, int j, double scalar);
+
+}
